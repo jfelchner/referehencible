055f92df5b3a2600b62fb18c82eacd136660a96b
Jeff Felchner <accounts+git@thekompanee.com>
2014-03-19 21:52:23 -0500

    Add a placeholder for a spec

---
+++ spec/lib/referehencible_spec.rb
@@ -0,0 +1,19 @@
+require 'rspectacular'
+require 'referehencible'
+
+class Referenced
+  include Referehencible
+end
+
+# describe  Referehencible do
+#   it { expect(subject).to     allow_value('a' * 32).for :guid }
+#   it { expect(subject).not_to allow_value('a' * 33).for :guid }
+#   it { expect(subject).not_to allow_value('y' * 32).for :guid }
+#
+#   it 'cannot create two tokenables with the same guid' do
+#     referenced = Referenced.new
+#     referenced guid: '3dd76acc3d4a2f2e33b5316500b8a484')
+#
+#     expect(subject).to validate_uniqueness_of(:guid)
+#   end
+# end
