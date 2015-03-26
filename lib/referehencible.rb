976e99ba7f2901c54d75f86b5315ef7b4ec14084
Jeff Felchner <accounts+git@thekompanee.com>
2015-03-26 00:18:13 -0500

    Style: Rubocop fixes

--- lib/referehencible.rb
+++ lib/referehencible.rb
@@ -4,55 +4,66 @@ require 'securerandom'
 module Referehencible
   DEFAULT_LENGTH = 36
 
+  # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength
   module ClassMethods
-    def referenced_by(*referenced_attributes)
+    def referenced_by(*referenced_attrs)
       default_options = {
         length: DEFAULT_LENGTH,
         type:   :uuid,
       }
 
-      referenced_attributes = referenced_attributes.each_with_object({}) do |referenced_attribute, transformed_attributes|
-                                case referenced_attribute
-                                when Symbol
-                                  transformed_attributes[referenced_attribute] = default_options
-                                when Hash
-                                  transformed_attributes.merge! referenced_attribute
-                                end
-                              end
+      referenced_attrs = \
+        referenced_attrs.each_with_object({}) do |referenced_attr, transformed_attr|
+          case referenced_attr
+          when Symbol
+            transformed_attr[referenced_attr] = default_options
+          when Hash
+            transformed_attr.merge! referenced_attr
+          end
+        end
 
-      referenced_attributes.each do |reference_attribute, options|
-        raise RuntimeError, "You attempted to pass in a length of #{options[:length]} for #{reference_attribute} in #{self.name}. Only even numbers are allowed." \
+      referenced_attrs.each do |reference_attribute, options|
+        fail "You attempted to pass in a length of #{options[:length]} for " \
+             "#{reference_attribute} in #{name}. Only even numbers are allowed." \
           if options[:length].odd?
 
-        validates       reference_attribute,
-                          presence:         true,
-                          uniqueness:       true,
-                          format:           {
-                            with:             /[a-f0-9\-]{#{options[:length]}}/ },
-                          length:           {
-                            is:               options[:length] }
+        validates reference_attribute,
+                  presence:   true,
+                  uniqueness: true,
+                  format:     {
+                    with:             /[a-f0-9\-]{#{options[:length]}}/ },
+                  length:     {
+                    is:               options[:length] }
 
         define_method(reference_attribute) do
-          send("generate_#{options[:type]}_guid", reference_attribute, options[:length] / 2)
+          send("generate_#{options[:type]}_guid",
+               reference_attribute,
+               options[:length] / 2)
         end
 
         define_singleton_method("by_#{reference_attribute}") do |guid_to_find|
           where(:"#{reference_attribute}" => guid_to_find).
-          first ||
-          unknown_reference_object
+            first ||
+            unknown_reference_object
         end
 
-        after_initialize lambda { send("generate_#{options[:type]}_guid", reference_attribute, options[:length] / 2) }
+        after_initialize lambda do
+          send("generate_#{options[:type]}_guid",
+               reference_attribute,
+               options[:length] / 2)
+        end
       end
 
       private
 
       define_method(:generate_hex_guid) do |reference_attribute, length|
-        read_attribute(reference_attribute) || write_attribute(reference_attribute, SecureRandom.hex(length))
+        read_attribute(reference_attribute) || write_attribute(reference_attribute,
+                                                               SecureRandom.hex(length))
       end
 
-      define_method(:generate_uuid_guid) do |reference_attribute, length|
-        read_attribute(reference_attribute) || write_attribute(reference_attribute, SecureRandom.uuid)
+      define_method(:generate_uuid_guid) do |reference_attribute, _length|
+        read_attribute(reference_attribute) || write_attribute(reference_attribute,
+                                                               SecureRandom.uuid)
       end
 
       define_singleton_method(:unknown_reference_object) do
@@ -62,6 +73,7 @@ module Referehencible
       end
     end
   end
+  # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength
 
   def self.included(base)
     base.extend ClassMethods
--- referehencible.gemspec
+++ referehencible.gemspec
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-$:.push File.expand_path("../lib", __FILE__)
+$LOAD_PATH.push File.expand_path('../lib', __FILE__)
 require 'referehencible/version'
 
 Gem::Specification.new do |s|
@@ -11,23 +11,23 @@ Gem::Specification.new do |s|
   s.version               = Referehencible::VERSION
   s.platform              = Gem::Platform::RUBY
 
-  s.authors               = %w{jfelchner m5rk}
-  s.email                 = 'support@chirrpy.com'
+  s.authors               = %w{jfelchner}
+  s.email                 = 'accounts+git@thekompanee.com'
   s.date                  = Time.now
   s.homepage              = 'https://github.com/chirrpy/referehencible'
 
-  s.summary               = %q{Enable unique reference numbers on all the things}
-  s.description           = %q{}
+  s.summary               = 'Enable unique reference numbers on all the things'
+  s.description           = ''
 
-  s.rdoc_options          = ["--charset = UTF-8"]
-  s.extra_rdoc_files      = %w[README.md LICENSE]
+  s.rdoc_options          = ['--charset = UTF-8']
+  s.extra_rdoc_files      = %w{README.md LICENSE}
 
-  s.executables           = Dir["{bin}/**/*"]
-  s.files                 = Dir["{app,config,db,lib}/**/*"] + %w{Rakefile README.md}
-  s.test_files            = Dir["{test,spec,features}/**/*"]
+  s.executables           = Dir['{bin}/**/*'].map    { |bin| File.basename(bin) }.
+                                             reject { |bin| %w{rails rspec rake setup deploy}.include? bin }
+  s.files                 = Dir['{app,config,db,lib,templates}/**/*'] + %w{Rakefile README.md LICENSE}
+  s.test_files            = Dir['{test,spec,features}/**/*']
   s.require_paths         = ['lib']
 
-  s.add_development_dependency 'rspec',                     '~> 3.0'
-  s.add_development_dependency 'rspectacular',              '~> 0.45.0'
-  s.add_development_dependency 'codeclimate-test-reporter', '~> 0.3.0'
+  s.add_development_dependency 'rspec',        ['~> 3.0']
+  s.add_development_dependency 'rspectacular', ['~> 0.45.0']
 end
